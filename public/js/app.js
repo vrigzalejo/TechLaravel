3cc8d246b6ca230dfdddeceea87df17d644f51a5
Vrigz Alejo <nuedu2012@gmail.com>
2016-03-17 03:04:30 +0800

    Done [removed unnecessary libraries]

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
 /**
  * Created by vrigzlinuxmint13 on 3/16/16.
  */
-angular.module('techTestApp', ['mainController', 'countryService', 'registerService', 'nya.bootstrap.select']);
+angular.module('techTestApp', ['mainController', 'countryService', 'registerService']);
--- public/views/index.php
+++ public/views/index.php
@@ -11,7 +11,6 @@
     <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
     <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.8/angular.min.js"></script>
 
-    <script src="/js/nya-bs-select.min.js"></script>
     <script src="/js/controllers/main.js"></script>
     <script src="/js/services/register.js"></script>
     <script src="/js/services/country.js"></script>
